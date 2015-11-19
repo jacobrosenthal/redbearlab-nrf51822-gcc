f320409d4d1ea51d676b5844b4442b881f093aac
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-11-19 01:51:23 -0500

    reset after upload

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
-openocd -f board/nordic_nrf51822_mkit.cfg -c "init; reset init; halt; nrf51 mass_erase; program $1; shutdown"
+openocd -f board/nordic_nrf51822_mkit.cfg -c "init; reset init; halt; nrf51 mass_erase; program $1; reset; exit"
 
